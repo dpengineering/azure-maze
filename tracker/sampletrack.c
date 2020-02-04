fd16a20156a201fa07fc6c14a22746c6f3bc0435
joe mama <haha@yahoo.org>
2020-02-04 12:33:53 -0800

    Copy simple-sample, modify to return joint info. does not work

--- tracker/sampletrack.c
+++ tracker/sampletrack.c
@@ -1,3 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,19 +16,47 @@
         exit(1);                                                                                         \
     }                                                                                                    \
 
+void print_joint_information(int i, k4abt_body_t body)
+{
+  k4a_float3_t position = body.skeleton.joints[i].position;
+  k4abt_joint_confidence_level_t confidence_level = body.skeleton.joints[i].confidence_level;
+  printf("Joint[%d]: Position[mm] ( %f, %f, %f ); Confidence Level (%d) \n",
+      i, position.v[0], position.v[1], position.v[2], confidence_level);
+}
+
+void print_body_index_map_middle_line(k4a_image_t body_index_map)
+{
+    uint8_t* body_index_map_buffer = k4a_image_get_buffer(body_index_map);
+
+    // Given body_index_map pixel type should be uint8, the stride_byte should be the same as width
+    // TODO: Since there is no API to query the byte-per-pixel information, we have to compare the width and stride to
+    // know the information. We should replace this assert with proper byte-per-pixel query once the API is provided by
+    // K4A SDK.
+    assert(k4a_image_get_stride_bytes(body_index_map) == k4a_image_get_width_pixels(body_index_map));
+
+    int middle_line_num = k4a_image_get_height_pixels(body_index_map) / 2;
+    body_index_map_buffer = body_index_map_buffer + middle_line_num * k4a_image_get_width_pixels(body_index_map);
+
+    printf("BodyIndexMap at Line %d:\n", middle_line_num);
+    for (int i = 0; i < k4a_image_get_width_pixels(body_index_map); i++)
+    {
+        printf("%u, ", *body_index_map_buffer);
+        body_index_map_buffer++;
+    }
+    printf("\n");
+}
+
 int main()
 {
-    k4a_device_t device = NULL;
-    VERIFY(k4a_device_open(0, &device), "Open K4A Device failed!");
+    k4a_device_configuration_t device_config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
+    device_config.depth_mode = K4A_DEPTH_MODE_NFOV_UNBINNED;
 
-    // Start camera. Make sure depth camera is enabled.
-    k4a_device_configuration_t deviceConfig = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
-    deviceConfig.depth_mode = K4A_DEPTH_MODE_NFOV_UNBINNED;
-    deviceConfig.color_resolution = K4A_COLOR_RESOLUTION_OFF;
-    VERIFY(k4a_device_start_cameras(device, &deviceConfig), "Start K4A cameras failed!");
+    k4a_device_t device;
+    VERIFY(k4a_device_open(0, &device), "Open K4A Device failed!");
+    VERIFY(k4a_device_start_cameras(device, &device_config), "Start K4A cameras failed!");
 
     k4a_calibration_t sensor_calibration;
-    VERIFY(k4a_device_get_calibration(device, deviceConfig.depth_mode, deviceConfig.color_resolution, &sensor_calibration),
+    VERIFY(k4a_device_get_calibration(device, device_config.depth_mode, K4A_COLOR_RESOLUTION_OFF, &sensor_calibration),
         "Get depth camera calibration failed!");
 
     k4abt_tracker_t tracker = NULL;
@@ -38,8 +71,12 @@ int main()
         if (get_capture_result == K4A_WAIT_RESULT_SUCCEEDED)
         {
             frame_count++;
+
+            printf("Start processing frame %d\n", frame_count);
+
             k4a_wait_result_t queue_capture_result = k4abt_tracker_enqueue_capture(tracker, sensor_capture, K4A_WAIT_INFINITE);
-            k4a_capture_release(sensor_capture); // Remember to release the sensor capture once you finish using it
+
+            k4a_capture_release(sensor_capture);
             if (queue_capture_result == K4A_WAIT_RESULT_TIMEOUT)
             {
                 // It should never hit timeout when K4A_WAIT_INFINITE is set.
@@ -56,25 +93,28 @@ int main()
             k4a_wait_result_t pop_frame_result = k4abt_tracker_pop_result(tracker, &body_frame, K4A_WAIT_INFINITE);
             if (pop_frame_result == K4A_WAIT_RESULT_SUCCEEDED)
             {
-                // Successfully popped the body tracking result. Start your processing
+                uint32_t num_bodies = k4abt_frame_get_num_bodies(body_frame);
+                printf("%u bodies are detected!\n", num_bodies);
 
-                size_t num_bodies = k4abt_frame_get_num_bodies(body_frame);
-                printf("%zu bodies are detected!\n", num_bodies);
+                k4abt_body_t body;
+                VERIFY(k4abt_frame_get_body_skeleton(body_frame, 1, &body.skeleton), "Get body from body frame failed!");
+                body.id = k4abt_frame_get_body_id(body_frame, 1);
+                //PROCESSING GOES HERE
+                printf("Body ID: %u\n", body.id);
+                //LEFT HAND: 8 RIGHT HAND: 15 LEFT ELBOW: 6 RIGHT ELBOW: 13
+                print_joint_information(8, body);
+                print_joint_information(6, body);
+                print_joint_information(15, body);
+                print_joint_information(13, body);
 
-                k4abt_frame_release(body_frame); // Remember to release the body frame once you finish using it
-            }
+              }
             else if (pop_frame_result == K4A_WAIT_RESULT_TIMEOUT)
             {
                 //  It should never hit timeout when K4A_WAIT_INFINITE is set.
                 printf("Error! Pop body frame result timeout!\n");
                 break;
             }
-            else
-            {
-                printf("Pop body frame result failed!\n");
-                break;
-            }
-        }
+          }
         else if (get_capture_result == K4A_WAIT_RESULT_TIMEOUT)
         {
             // It should never hit time out when K4A_WAIT_INFINITE is set.
@@ -87,7 +127,7 @@ int main()
             break;
         }
 
-    } while (frame_count < 100);
+    } while (true);
 
     printf("Finished body tracking processing!\n");
 
