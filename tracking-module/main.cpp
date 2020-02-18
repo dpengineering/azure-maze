0c22b6f53f92d3a41f2a4b59a9ab148456653cd2
Andrew Xie <andrewxie@dpengineering.org>
2020-02-18 10:38:34 -0800

    Add C++ Angle Calculation

--- tracking-module/main.cpp
+++ tracking-module/main.cpp
@@ -6,46 +6,15 @@
 #include <assert.h>
 #include <iostream>
 
+#include <math.h>
+
 #include <k4a/k4a.hpp>
 #include <k4abt.hpp>
 
-void print_body_information(k4abt_body_t body)
-{
-    std::cout << "Body ID: " << body.id << std::endl;
-    for (int i = 0; i < (int)K4ABT_JOINT_COUNT; i++)
-    {
-        k4a_float3_t position = body.skeleton.joints[i].position;
-        k4a_quaternion_t orientation = body.skeleton.joints[i].orientation;
-        k4abt_joint_confidence_level_t confidence_level = body.skeleton.joints[i].confidence_level;
-        printf("Joint[%d]: Position[mm] ( %f, %f, %f ); Orientation ( %f, %f, %f, %f); Confidence Level (%d)  \n",
-            i, position.v[0], position.v[1], position.v[2], orientation.v[0], orientation.v[1], orientation.v[2], orientation.v[3], confidence_level);
-    }
-}
-
-void print_body_index_map_middle_line(k4a::image body_index_map)
-{
-    uint8_t* body_index_map_buffer = body_index_map.get_buffer();
-
-    // Given body_index_map pixel type should be uint8, the stride_byte should be the same as width
-    // TODO: Since there is no API to query the byte-per-pixel information, we have to compare the width and stride to
-    // know the information. We should replace this assert with proper byte-per-pixel query once the API is provided by
-    // K4A SDK.
-    assert(body_index_map.get_stride_bytes() == body_index_map.get_width_pixels());
-
-    int middle_line_num = body_index_map.get_height_pixels() / 2;
-    body_index_map_buffer = body_index_map_buffer + middle_line_num * body_index_map.get_width_pixels();
-
-    std::cout << "BodyIndexMap at Line " << middle_line_num << ":" << std::endl;
-    for (int i = 0; i < body_index_map.get_width_pixels(); i++)
-    {
-        std::cout << (int)*body_index_map_buffer << ", ";
-        body_index_map_buffer++;
-    }
-    std::cout << std::endl;
-}
 
 int main()
 {
+    int portNum = 7266;
     try
     {
         k4a_device_configuration_t device_config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
@@ -81,13 +50,27 @@ int main()
                     uint32_t num_bodies = body_frame.get_num_bodies();
                     std::cout << num_bodies << " bodies are detected!" << std::endl;
 
+                    uint32_t c = 0;
                     for (uint32_t i = 0; i < num_bodies; i++)
                     {
+                        k4abt_body_t body;
+                        k4abt_body_t cbody;
                         k4abt_body_t body = body_frame.get_body(i);
-                        print_body_information(body);
+                        k4abt_body_t cbody = body_frame.get_body(c);
+
+                        if (body.skeleton.joints[2].position.v[2] < cbody.skeleton.joints[2].position.v[2])
+                        {
+                          c = i
+                        }
                     }
 
-                    k4a::image body_index_map = body_frame.get_body_index_map();
+                    k4abt_body_t final_body = body_frame.get_body(c); //final body
+                    float radangle = atan2 (final_body.skeleton.joints[8].position.v[1] - final_body.skeleton.joints[15].position.v[1], final_body.skeleton.joints[8].position.v[0] - final_body.skeleton.joints[15].position.v[0]);
+                    int angle = radangle * 180 / 3.14;
+
+                    std::cout << angle << std::endl;
+
+
 
                 }
                 else
