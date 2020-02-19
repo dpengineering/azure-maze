2b3ad23b5ae2c32526c4e43bfb6aec9ecd9df7ec
Andrew Xie <andrewxie@dpengineering.org>
2020-02-19 11:47:30 -0800

    Simplify for future use

--- cpp-track/main.cpp
+++ cpp-track/main.cpp
@@ -9,41 +9,6 @@
 #include <k4a/k4a.hpp>
 #include <k4abt.hpp>
 
-void print_body_information(k4abt_body_t body)
-{
-    std::cout << "Body ID: " << body.id << std::endl;
-    for (int i = 0; i < (int)K4ABT_JOINT_COUNT; i++)
-    {
-        k4a_float3_t position = body.skeleton.joints[i].position;
-        k4a_quaternion_t orientation = body.skeleton.joints[i].orientation;
-        k4abt_joint_confidence_level_t confidence_level = body.skeleton.joints[i].confidence_level;
-        printf("Joint[%d]: Position[mm] ( %f, %f, %f ); Orientation ( %f, %f, %f, %f); Confidence Level (%d)  \n",
-            i, position.v[0], position.v[1], position.v[2], orientation.v[0], orientation.v[1], orientation.v[2], orientation.v[3], confidence_level);
-    }
-}
-
-void print_body_index_map_middle_line(k4a::image body_index_map)
-{
-    uint8_t* body_index_map_buffer = body_index_map.get_buffer();
-
-    // Given body_index_map pixel type should be uint8, the stride_byte should be the same as width
-    // TODO: Since there is no API to query the byte-per-pixel information, we have to compare the width and stride to
-    // know the information. We should replace this assert with proper byte-per-pixel query once the API is provided by
-    // K4A SDK.
-    assert(body_index_map.get_stride_bytes() == body_index_map.get_width_pixels());
-
-    int middle_line_num = body_index_map.get_height_pixels() / 2;
-    body_index_map_buffer = body_index_map_buffer + middle_line_num * body_index_map.get_width_pixels();
-
-    std::cout << "BodyIndexMap at Line " << middle_line_num << ":" << std::endl;
-    for (int i = 0; i < body_index_map.get_width_pixels(); i++)
-    {
-        std::cout << (int)*body_index_map_buffer << ", ";
-        body_index_map_buffer++;
-    }
-    std::cout << std::endl;
-}
-
 int main()
 {
     try
@@ -84,17 +49,15 @@ int main()
                     for (uint32_t i = 0; i < num_bodies; i++)
                     {
                         k4abt_body_t body = body_frame.get_body(i);
-                        print_body_information(body);
-                    }
-
-                    k4a::image body_index_map = body_frame.get_body_index_map();
-                    if (body_index_map != nullptr)
-                    {
-                        print_body_index_map_middle_line(body_index_map);
-                    }
-                    else
-                    {
-                        std::cout << "Error: Failed to generate bodyindex map!" << std::endl;
+                        std::cout << "Body ID: " << body.id << std::endl;
+                        for (int i = 0; i < (int)K4ABT_JOINT_COUNT; i++)
+                        {
+                            k4a_float3_t position = body.skeleton.joints[i].position;
+                            k4a_quaternion_t orientation = body.skeleton.joints[i].orientation;
+                            k4abt_joint_confidence_level_t confidence_level = body.skeleton.joints[i].confidence_level;
+                            printf("Joint[%d]: Position[mm] ( %f, %f, %f ); Orientation ( %f, %f, %f, %f); Confidence Level (%d)  \n",
+                                i, position.v[0], position.v[1], position.v[2], orientation.v[0], orientation.v[1], orientation.v[2], orientation.v[3], confidence_level);
+                        }
                     }
                 }
                 else
@@ -111,7 +74,7 @@ int main()
                 break;
             }
         }
-        
+
         std::cout << "Finished body tracking processing!" << std::endl;
 
     }
