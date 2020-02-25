3ed2427fa0c2ed645c3f6f1568632b0d310482ca
joe mama <haha@yahoo.org>
2020-02-25 09:45:05 -0800

    Kivy attempts

--- kivy-gui/main.py
+++ kivy-gui/main.py
@@ -5,24 +5,14 @@ from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-from pidev.MixPanel import MixPanel
-from pidev.kivy.PassCodeScreen import PassCodeScreen
-from pidev.kivy.PauseScreen import PauseScreen
-from pidev.kivy import DPEAButton
-from pidev.kivy import ImageButton
 
 from ex5 import *
 
-
-MIXPANEL_TOKEN = "x"
-MIXPANEL = MixPanel("Project Name", MIXPANEL_TOKEN)
-
 SCREEN_MANAGER = ScreenManager()
 MAIN_SCREEN_NAME = 'main'
-ADMIN_SCREEN_NAME = 'admin'
 
 
-class ProjectNameGUI(App):
+class AzureMaze(App):
     """
     Class to handle running the GUI Application
     """
@@ -39,81 +29,15 @@ Window.clearcolor = (1, 1, 1, 1)  # White
 
 
 class MainScreen(Screen):
-    """
-    Class to handle the main screen and its associated touch events
-    """
-
-    def pressed(self):
-        """
-        Function called on button touch event for button with id: testButton
-        :return: None
-        """
-        PauseScreen.pause(pause_scene_name='pauseScene', transition_back_scene='main', text="Test", pause_duration=5)
-
-    def admin_action(self):
-        """
-        Hidden admin button touch event. Transitions to passCodeScreen.
-        This method is called from pidev/kivy/PassCodeScreen.kv
-        :return: None
-        """
-        SCREEN_MANAGER.current = 'passCode'
-
-
-class SecondScreen(Screen):
     pass
 
-class AdminScreen(Screen):
-    """
-    Class to handle the AdminScreen and its functionality
-    """
 
-    def __init__(self, **kwargs):
-        """
-        Load the AdminScreen.kv file. Set the necessary names of the screens for the PassCodeScreen to transition to.
-        Lastly super Screen's __init__
-        :param kwargs: Normal kivy.uix.screenmanager.Screen attributes
-        """
-        Builder.load_file('AdminScreen.kv')
-
-        PassCodeScreen.set_admin_events_screen(ADMIN_SCREEN_NAME)  # Specify screen name to transition to after correct password
-        PassCodeScreen.set_transition_back_screen(MAIN_SCREEN_NAME)  # set screen name to transition to if "Back to Game is pressed"
-
-        super(AdminScreen, self).__init__(**kwargs)
-
-    @staticmethod
-    def transition_back():
-        """
-        Transition back to the main screen
-        :return:
-        """
-        SCREEN_MANAGER.current = MAIN_SCREEN_NAME
-
-    @staticmethod
-    def shutdown():
-        """
-        Shutdown the system. This should free all steppers and do any cleanup necessary
-        :return: None
-        """
-        os.system("sudo shutdown now")
-
-    @staticmethod
-    def exit_program():
-        """
-        Quit the program. This should free all steppers and do any cleanup necessary
-        :return: None
-        """
-        quit()
 """
 Widget additions
 """
 
 Builder.load_file('main.kv')
 SCREEN_MANAGER.add_widget(MainScreen(name=MAIN_SCREEN_NAME))
-SCREEN_MANAGER.add_widget(SecondScreen(name="two"))
-SCREEN_MANAGER.add_widget(PassCodeScreen(name='passCode'))
-SCREEN_MANAGER.add_widget(PauseScreen(name='pauseScene'))
-SCREEN_MANAGER.add_widget(AdminScreen(name=ADMIN_SCREEN_NAME))
-
 """
 MixPanel
 """
@@ -134,4 +58,4 @@ def send_event(event_name):
 if __name__ == "__main__":
     # send_event("Project Initialized")
     # Window.fullscreen = 'auto'
-    ProjectNameGUI().run()
+    AzureMaze().run()
