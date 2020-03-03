78933e07c6ecab4bc12214286a113496a192336e
joe mama <haha@yahoo.org>
2020-03-03 09:51:42 -0800

    Mouse control works

--- testing/mouse-control/mouse.sh
+++ testing/mouse-control/mouse.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
+echo "Starting mouse limiter."
 while true
 do
 
 eval $(xdotool getmouselocation --shell)
 
-if [ $X -gt 1258 ]
+if [ $X -gt 1265 ]
 then
-xdotool mousemove 1258, $Y
+xdotool mousemove 1265, $Y
 fi
 
 done
